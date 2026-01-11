8897a69bf514d199a07fe9bec774c3680510b20c
antranix <andxdd@gmail.com>
2026-01-11 14:19:59 -0500

    ...

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,9 @@
 import * as vscode from "vscode";
 import { registerTodo } from "./todo/index";
-import { registerChangelog } from "./changelog/index";
 
 export function activate(context: vscode.ExtensionContext) {
 
   registerTodo(context);
-
-  registerChangelog(context);
 
 }
 
